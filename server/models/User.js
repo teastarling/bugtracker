e49bab638f265d3f3caf13455ff18a2b42a516e3
Tyler Starling <tyler.a.starling@gmail.com>
2021-09-23 08:50:21 -0400

    fixed mismatched connection file content and added model index as well as user model content

---
+++ server/models/User.js
@@ -0,0 +1,47 @@
+const { Schema, model } = require('mongoose');
+const bcrypt = require('bcrypt');
+// user model including item collection via item _id
+const userSchema = new Schema({
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    match: [/.+@.+\..+/, 'Must match an email address!'],
+  },
+  password: {
+    type: String,
+    required: true,
+    minlength: 5,
+  },
+  project: [{
+    type: Schema.ObjectId,
+    ref: 'Project',
+  }],
+  issue: [{
+    type: Schema.ObjectId,
+    ref: 'Issue',
+  }]
+});
+// user password hashed on the way to the database via bcrypt
+userSchema.pre('save', async function (next) {
+  if (this.isNew || this.isModified('password')) {
+    const saltRounds = 10;
+    this.password = await bcrypt.hash(this.password, saltRounds);
+  }
+
+  next();
+});
+// checking if password is correct via bcrypt unhashing 
+userSchema.methods.isCorrectPassword = async function (password) {
+  return bcrypt.compare(password, this.password);
+};
+
+const User = model('User', userSchema);
+
+module.exports = User;
---
+++ server/models/index.js
@@ -0,0 +1,5 @@
+const Issue = require('./Issue');
+const Project = require('./Project');
+const User = require('./User');
+// exporting the item and user models
+module.exports = {Issue, User, Project };
