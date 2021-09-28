8bd74fa1b9860577463f7ee48123e9b6d17ce4ea
Tyler Starling <tyler.a.starling@gmail.com>
2021-09-28 13:33:48 -0400

    working on typedefs

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,47 +1,53 @@
 const { gql } = require('apollo-server-express');
 // import graphql, include all fields for item and user models as well as auth token, and queries and mutations according to what information they are taking in and responding with
-// const typeDefs = gql`
-//   type User {
-//     _id: ID!
-//     username: String!
-//     email: String!
-//     password: String!
-//     item: [Item]
-//   }
+const typeDefs = gql`
+  type User {
+    _id: ID!
+    username: String!
+    email: String!
+    password: String!
+    issues: [Issue]
+    projects: [Project]
+  }
 
-//   type Item {
-//     _id: ID!
-//     genre: String!
-//     name: String!
-//     location: String!
-//     condition: String!
-//     description: String
-//     image_id: String!
-//     created_at: String
-//     user: String!
-//     email: String!
-//   }
+  type Issue {
+    _id: ID!
+    name: String!
+    status: String!
+    description: String
+    user: String!
+    project: String!
+  }
 
-//   type Auth {
-//     token: ID!
-//     user: User
-//   }
+  type Project {
+    _id: ID!
+    name: String!
+    status: String!
+    description: String
+    user: String!
+    issues: 
+  }
 
-//   type Query {
-//     users: [User]
-//     user(username: String!): User
-//     items(username: String): [Item]
-//     genreItems(genre: String!): [Item]
-//     item(itemId: ID!): Item
-//     me: User
-//   }
+  type Auth {
+    token: ID!
+    user: User
+  }
 
-//   type Mutation {
-//     addUser(username: String!, email: String!, password: String!): Auth
-//     login(email: String!, password: String!): Auth
-//     addItem(name: String!, genre: String!, location: String!, condition: String!, description: String, image_id: String!): Item
-//     removeItem (itemId: ID!): Item
-//   }
+  type Query {
+    users: [User]
+    user(username: String!): User
+    items(username: String): [Item]
+    genreItems(genre: String!): [Item]
+    item(itemId: ID!): Item
+    me: User
+  }
+
+  type Mutation {
+    addUser(username: String!, email: String!, password: String!): Auth
+    login(email: String!, password: String!): Auth
+    addItem(name: String!, genre: String!, location: String!, condition: String!, description: String, image_id: String!): Item
+    removeItem (itemId: ID!): Item
+  }
 `;
 // export typedefs
 module.exports = typeDefs;
